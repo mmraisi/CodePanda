b6bce89fd8e8d03bcd2b4b350f038fb95b5502a5
m-mraisi <mahmodmreasi@gmail.com>
2022-11-15 23:08:35 -0500

    Initial commit

---
+++ app/src/main/java/com/test/project_g10/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.project_g10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

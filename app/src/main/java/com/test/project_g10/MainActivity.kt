7b7ad18f1825761a5e2fa8ced52e7c74b529baee
m-mraisi <mahmodmreasi@gmail.com>
2022-11-15 23:15:51 -0500

    Add viewBinding

--- app/src/main/java/com/test/project_g10/MainActivity.kt
+++ app/src/main/java/com/test/project_g10/MainActivity.kt
@@ -2,10 +2,17 @@ package com.test.project_g10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.project_g10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+     val TAG = this@MainActivity.toString()
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView((binding.root))
     }
 }

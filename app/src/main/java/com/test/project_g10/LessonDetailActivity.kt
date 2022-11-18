1dbf1ca95cb1f5dba2f1880d893b55ac551ec247
aratrika <aratrika.mukherjee26@gmail.com>
2022-11-18 01:40:27 -0500

    Added splash screen and save notes to datasource

--- app/src/main/java/com/test/project_g10/LessonDetailActivity.kt
+++ app/src/main/java/com/test/project_g10/LessonDetailActivity.kt
@@ -7,25 +7,45 @@ import java.io.Serializable
 
 class LessonDetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityLessonDetailBinding
+    lateinit var dataSource: DataSource
+    lateinit var lessonsArrayList:ArrayList<Lesson>
     var TAG = this@LessonDetailActivity.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLessonDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        dataSource = DataSource.getInstance()
         if (intent != null) {
             val lessonFromScreen1:Serializable? = intent.getSerializableExtra("EXTRA_LESSON")
             if (lessonFromScreen1 != null) {
                 val lesson:Lesson? = lessonFromScreen1 as? Lesson
-
+                lessonsArrayList = dataSource.lessonsArrayList
                 if (lesson != null) {
                     // output to the screen
                     binding.tvLessonTitle.setText("${lesson.id}. ${lesson.title}")
                     binding.tvLessonDuration.setText(lesson.duration)
                     binding.tvLessonInfo.setText(lesson.lessonInfo)
+                    if (lesson.notes != "") {
+                        binding.edtNotes.setText(lesson.notes)
+                    }
+                }
+
+
+                binding.btnSaveNotes.setOnClickListener {
+                    val notesFromUser = binding.edtNotes.text
+                    for (eachLesson in lessonsArrayList) {
+                        if (lesson != null) {
+                            if (eachLesson.id == lesson.id) {
+                                eachLesson.notes = notesFromUser.toString()
+                            }
+
+                        }
+                    }
                 }
             }
         }
+
+
     }
 }
--- app/src/main/java/com/test/project_g10/SplashScreenActivity.kt
+++ app/src/main/java/com/test/project_g10/SplashScreenActivity.kt
@@ -7,20 +7,23 @@ import android.os.Handler
 import com.test.project_g10.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
-
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
     lateinit var binding:ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        Handler().postDelayed(Runnable {
-//
-//            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
-//            startActivity(i)
-//            // close this activity
-//            finish()
-//        }, 3000)
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
